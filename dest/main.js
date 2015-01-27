4bf255bf4d80deace6d80f9a0c52652c39762a20
agconti <agc11d@gmail.com>
2015-01-26 22:29:27 -0500

    feat(js/main): naive prototype working with time

--- dest/main.js
+++ dest/main.js
@@ -1,15 +1,20 @@
-var time = ['hours', 'minutes', 'seconds']
+var now = new Date()
+  , time = [ now.getHours()
+           , now.getMinutes()
+           , now.getSeconds()
+           ]
+
 var svg = d3.select('.clock')
   .append('svg')
   .append('g')
 
-svg.selectAll('rect')
+var rects = svg.selectAll('rect')
   .data(time).enter()
   .append('rect')
   .attr('x', function(data, i){ return i * 101})
   .attr('y', 0)
   .attr('width', 100)
-  .attr('height', function(){ return  400 * Math.pow(Math.random(), 2) })
+  .attr('height', function(data){ return data})
 
 
     
