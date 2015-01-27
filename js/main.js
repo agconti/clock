e128478de20cec2c167ea2d0f34426d95d64b686
agconti <agc11d@gmail.com>
2015-01-26 22:23:03 -0500

    feat(js/main): basic naive example

--- js/main.js
+++ js/main.js
@@ -6,4 +6,10 @@ var svg = d3.select('.clock')
 svg.selectAll('rect')
   .data(time).enter()
   .append('rect')
+  .attr('x', function(data, i){ return i * 101})
+  .attr('y', 0)
+  .attr('width', 100)
+  .attr('height', function(){ return  400 * Math.pow(Math.random(), 2) })
+
+
     
